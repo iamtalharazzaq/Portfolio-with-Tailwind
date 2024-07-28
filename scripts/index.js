3de2eb48b758078dfaa7501c6232cc18ddcae724
Talha Razzaq <talha.abdulrazzaq381@gmail.com>
2024-07-29 00:05:39 +0500

    4 - About

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,18 @@
-const sideMenu = document.querySelector('#side')
+const sideMenu = document.querySelector("#side");
 
 function openMenu() {
-    sideMenu.style.transform = 'translateX(-16rem)'
+  sideMenu.style.transform = "translateX(-16rem)";
 }
 
 function closeMenu() {
-    sideMenu.style.transform = 'translateX(16rem)'
+  sideMenu.style.transform = "translateX(16rem)";
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const wheelScroll = document.getElementById("scrollContainer");
+    wheelScroll.addEventListener("wheel", (e) => {
+        e.preventDefault();
+        wheelScroll.scrollLeft += e.deltaY; // Convert vertical scroll to horizontal scroll
+    });
+});
+
