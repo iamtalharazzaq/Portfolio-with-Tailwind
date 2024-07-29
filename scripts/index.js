cc0c306ea54b673fd96a97791b6dc466535a3bed
Talha Razzaq <talha.abdulrazzaq381@gmail.com>
2024-07-30 03:05:27 +0500

    5 - Added Scroll button with progress

--- scripts/index.js
+++ scripts/index.js
@@ -8,11 +8,25 @@ function closeMenu() {
   sideMenu.style.transform = "translateX(16rem)";
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    const wheelScroll = document.getElementById("scrollContainer");
-    wheelScroll.addEventListener("wheel", (e) => {
-        e.preventDefault();
-        wheelScroll.scrollLeft += e.deltaY; // Convert vertical scroll to horizontal scroll
-    });
-});
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  if (pos > 100) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(#588b76 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
 
